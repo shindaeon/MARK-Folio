016cdf69d89f01aa78ad651f5328cf88fafe530f
Dion Mark Corales <navybl4456@gmail.com>
2024-06-12 19:31:30 +0800

    Added Carousel

--- mark-folio/src/js/main.js
+++ mark-folio/src/js/main.js
@@ -3,6 +3,7 @@ import javascriptLogo from "../assets/javascript.svg";
 import viteLogo from "../vite.svg";
 import { navBar } from "./components/navbar";
 import Router from "./routes.js";
+import * as bootstrap from 'bootstrap'
 
 document.querySelector("#app").innerHTML = `
   <nav id="navbar" class="position-sticky fixed-top">
